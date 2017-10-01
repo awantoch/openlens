eae9cb8f4bd839afdc46cb6b67768e7b7f0088c0
jiyuu-jin <jiyuu.llc@gmail.com>
2017-10-01 02:01:13 -0400

    added minecraft model lolz

--- imports/ui/components/index.js
+++ imports/ui/components/index.js
@@ -1,18 +1,24 @@
 import React from 'react'
 
 import {Link} from 'react-router-dom';
+import {Entity, Scene} from 'aframe-react';
 
 
 const IndexPage = () =>({
     render(){
+        const code4pa = 'skins/code4pa2.png';
+        const saiyan = 'skins/saiyan.png';
+        return (
+            <div className="App">
+                <Scene artoolkit={{sourceType: 'webcam', trackingMethod: 'best'}}>
 
-        return(
-            <div>
-
-                <center><h1>Open Lens</h1></center>
-
+                    <a-anchor>
+                        <a-entity minecraft={`heightMeter: 1; skinUrl: ${code4pa}`} minecraft-head-anim="yes" minecraft-body-anim="hiwave" material='opacity: 0.5' />
+                    </a-anchor>
+                    <a-camera-static preset="hiro" />
+                </Scene>
             </div>
-        )
+        );
     },
 
     componentDidMount(){
