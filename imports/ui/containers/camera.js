cef289bcac83a825095456a6289ef0f741135a4e
Alec Wantoch <alec@wantoch.net>
2017-11-01 04:35:24 -0400

    added container for future query

---
+++ imports/ui/containers/camera.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {composeWithTracker} from 'react-komposer';
+import Camera from '../components/camera.jsx';
+
+const composer = (props, onData) => {
+        const pois = [
+        {
+            name: '1',
+            lat: '40.261811',
+            long: '-76.880295'
+        },
+        {
+            name: '2',
+            lat: '40.258458',
+            long: '-76.879980'
+        },
+        {
+            name: '3',
+            lat: '40.258373',
+            long: '-76.877753'
+        },
+        {
+            name: '4',
+            lat: '40.259832',
+            long: '-76.880817'
+        }
+    ];
+        onData(null, {pois});
+};
+
+export default composeWithTracker(composer)(Camera);
