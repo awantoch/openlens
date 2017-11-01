c4ee611c237dc06fe4126a4cc4290280ac6150bb
Alec Wantoch <alec@wantoch.net>
2017-11-01 02:49:42 -0400

    testing poi component

--- imports/ui/components/poi.jsx
+++ imports/ui/components/poi.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 
 const POI = (props) => ({
-
-	const coords = "longitude: " + {props.long} + "; latitude: " + {props.lat} + ";"
-	
 	render() {
+		const coords = "longitude: " + {props.long} + "; latitude: " + {props.lat} + ";"
 		return <a-sphere gps-place={coords}></a-sphere>
 	}
 });
