f532efd7aee9ff2f38c9e7cfbf21e114d58a8960
Alec Wantoch <alec@wantoch.net>
2017-11-02 01:29:17 -0400

    testing hover event

--- imports/ui/components/camera.js
+++ imports/ui/components/camera.js
@@ -38,7 +38,7 @@ class Camera extends Component {
                     </a-entity>
                 </a-camera>
                     {pois.map((poi)=>{
-                        return <a-tetrahedron id={poi.lat+poi.long} event-set__enter="_event: mouseenter; color: #8FF7FF" cursor-listener color="#FF926B" radius="3" key={poi.name} gps-Place={"longitude: " + poi.long + "; latitude: " + poi.lat}></a-tetrahedron>
+                        return <a-tetrahedron id={poi.lat+poi.long} cursor-listener color="#FF926B" radius="3" key={poi.name} gps-Place={"longitude: " + poi.long + "; latitude: " + poi.lat}></a-tetrahedron>
                     })}
                 </a-scene>
             </div>
@@ -49,7 +49,7 @@ class Camera extends Component {
 
         AFRAME.registerComponent('cursor-listener', {
           init: function () {
-            this.el.addEventListener('click', function (evt) {
+            this.el.addEventListener('fusing', function (evt) {
               alert('I was clicked at: ', evt.detail);
             });
           }})
