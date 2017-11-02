8f6896613cab4fe9777a3bc2bc68214032da9d24
Alec Wantoch <alec@wantoch.net>
2017-11-01 22:16:09 -0400

    adjusting...

--- imports/ui/components/camera.js
+++ imports/ui/components/camera.js
@@ -33,7 +33,7 @@ class Camera extends Component {
                 <a-camera id="camera" user-height="1.6" gps-position compass-rotation>
                     <a-entity Cursor="fuse: false"
                         Position="0 0 -3"
-                        Geometry="primitive: ring; radiusInner: 0.02; radiusOuter: 0.03"
+                        Geometry="primitive: ring; radiusInner: 0.05; radiusOuter: 0.06"
                         Material="color: purple; shader: flat">
                     </a-entity>
                 </a-camera>
