e659397acaee7a898d5d00157a4a2bd191c04ce4
jiyuu-jin <jiyuu.llc@gmail.com>
2017-11-08 02:13:06 -0500

    Detect android navigation

--- imports/ui/components/markerModal.js
+++ imports/ui/components/markerModal.js
@@ -2,17 +2,32 @@ import React, {Component} from 'react'
 
 class Modal extends Component {
 
-    close() {
+    constructor(props) {
+        super(props);
+        this.close = this.close.bind(this)
+    }
 
+    close() {
         if (this.props.onClose) {
             this.props.onClose()
+        }
+    }
+
+    launchNav(){
+        let ua = navigator.userAgent.toLowerCase();
+        let isAndroid = ua.indexOf("android") > -1;
+        if(isAndroid) {
+            alert("Android Gps");
+            close()
+        }else{
+            close()
         }
     }
 
     renderNavIcon(){
         if (this.props.loc) {
             return (
-                <a onClick={this.close.bind(this)} href={"https://www.google.com/maps/?q="+this.props.loc[1]+","+this.props.loc[0]}><i className="modal-action modal-close btn-flat blue lighten-2 fa fa-2x fa-location-arrow" aria-hidden="true"></i></a>
+                <a onClick={this.launchNav().bind(this)} href={"https://www.google.com/maps/?q="+this.props.loc[1]+","+this.props.loc[0]}><i className="modal-action modal-close btn-flat blue lighten-2 fa fa-2x fa-location-arrow" aria-hidden="true"></i></a>
             )
         }
     }
